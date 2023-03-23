10ec0641440b2fae1e3e0046490485f163959063
Euclécio Josias Rodrigues <euclecio.dev@gmail.com>
2023-03-23 17:21:41 -0300

    Script define ingressRoute e puxa arquivos dos templates

--- generate-iac-files.sh
+++ generate-iac-files.sh
@@ -32,9 +32,30 @@ EOF
 echo "::endgroup::"
 
 echo "::group::Criando values files do Helm para '$APP_NAME'"
-cp -r ../chart/values*.yaml ./
 
+fetch_template_file() {
+  curl -s "https://x-access-token:$ADMIN_TOKEN@raw.githubusercontent.com/betrybe/infrastructure-templates/main/helm-values-files/$1" -O
+}
+fetch_template_file values.yaml
+fetch_template_file values-homologation.yaml
+fetch_template_file values-production.yaml
+fetch_template_file values-staging.yaml
+fetch_template_file values-preview-apps.yaml
+
+# Adc nome do projeto nos values files
 find ./*.yaml -type f -exec sed -i "s/APP_NAME/$APP_NAME/g" {} \;
+
+# Configura ingressRoute default
+ingress_route_subdomain="api"
+ingress_route_path_prefix="&& PathPrefix('/$APP_NAME')"
+if [[ ! -z "$BETRYBE_SUBDOMAIN" ]]; then
+  ingress_route_subdomain=$BETRYBE_SUBDOMAIN
+  ingress_route_path_prefix=""
+fi
+
+find ./*.yaml -type f -exec sed -i "s/INGRESS_ROUTE_PATH_PREFIX/$ingress_route_path_prefix/g" {} \;
+find ./*.yaml -type f -exec sed -i "s/INGRESS_ROUTE_SUBDOMAIN/$ingress_route_subdomain/g" {} \;
+
 echo "::endgroup::"
 
 echo "::group::Criando nova branch, fazendo commit e push no repositório @$GITHUB_REPOSITORY"
